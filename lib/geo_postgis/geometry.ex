260290e8bbffed68dcafc5cad6751c8c772a11f8
Mariusz Morawski <marmor157pl@gmail.com>
2024-04-30 15:39:53 +0200

    Do not raise on unsuccessful cast (#156)
    * Do not raise on unsuccessful cast
    
    * Fix binary casting
    
    * Update lib/geo_postgis/geometry.ex
    
    * Return error from `Geo.JSON.decode/1`
    
    * Group `cast/1` together

--- lib/geo_postgis/geometry.ex
+++ lib/geo_postgis/geometry.ex
@@ -83,18 +83,32 @@ if Code.ensure_loaded?(Ecto.Type) do
     def cast(%struct{} = geom) when struct in @geometries, do: {:ok, geom}
 
     def cast(%{"type" => type, "coordinates" => _} = geom) when type in @types do
-      {:ok, Geo.JSON.decode!(geom)}
+      do_cast(geom)
     end
 
     def cast(%{"type" => "GeometryCollection", "geometries" => _} = geom) do
-      {:ok, Geo.JSON.decode!(geom)}
+      do_cast(geom)
     end
 
     def cast(geom) when is_binary(geom) do
-      {:ok, geom |> Geo.PostGIS.Config.json_library().decode!() |> Geo.JSON.decode!()}
+      do_cast(geom)
     end
 
     def cast(_), do: :error
+
+    defp do_cast(geom) when is_binary(geom) do
+      case Geo.PostGIS.Config.json_library().decode(geom) do
+        {:ok, geom} when is_map(geom) -> do_cast(geom)
+        {:error, reason} -> {:error, [message: "failed to decode JSON", reason: reason]}
+      end
+    end
+
+    defp do_cast(geom) do
+      case Geo.JSON.decode(geom) do
+        {:ok, result} -> {:ok, result}
+        {:error, reason} -> {:error, [message: "failed to decode GeoJSON", reason: reason]}
+      end
+    end
 
     def embed_as(_), do: :self
 
