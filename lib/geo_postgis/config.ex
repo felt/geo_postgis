f55cc0a37924f075024dc33213ab2b923ab947da
Matt Lewis <domattthings@gmail.com>
2025-03-23 19:29:08 -0400

    Use build in JSON encoder/decoder by default

--- lib/geo_postgis/config.ex
+++ lib/geo_postgis/config.ex
@@ -1,5 +1,11 @@
 defmodule Geo.PostGIS.Config do
+  if Code.ensure_loaded?(JSON) do
+    @default_json_library JSON
+  else
+    @default_json_library Poison
+  end
+
   def json_library do
-    Application.get_env(:geo_postgis, :json_library, Poison)
+    Application.get_env(:geo_postgis, :json_library, @default_json_library)
   end
 end
