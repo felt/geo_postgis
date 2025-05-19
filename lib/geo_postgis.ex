680781c87ede5192a8bb8170c8acf4df3f6bc3b7
Christopher Varjas <cvolunteer@proton.me>
2025-05-19 07:46:18 -0400

    Add ST_LineMerge/1 fragment (#243)
    * add macro
    
    * add tests
    
    * format
    
    * update test block

--- lib/geo_postgis.ex
+++ lib/geo_postgis.ex
@@ -459,4 +459,8 @@ defmodule Geo.PostGIS do
   defmacro st_node(geometry) do
     quote do: fragment("ST_Node(?)", unquote(geometry))
   end
+
+  defmacro st_line_merge(geometry) do
+    quote do: fragment("ST_LineMerge(?)", unquote(geometry))
+  end
 end
