a99b9add6b198ac189447fe22838cf2ab5a167f2
Christopher Varjas <cvolunteer@proton.me>
2025-05-22 21:39:48 -0400

    add more tests

--- test/geometry_test.exs
+++ test/geometry_test.exs
@@ -32,5 +32,57 @@ defmodule Geo.PostGIS.GeometryCastTest do
       assert point.coordinates == {30.0, -90.0}
       assert point.srid == 4326
     end
+
+    test "cast GeometryCollection from map with key => value syntax" do
+      collection_map = %{
+        "type" => "GeometryCollection",
+        "geometries" => [
+          %{
+            "type" => "Point",
+            "coordinates" => [30.0, -90.0]
+          },
+          %{
+            "type" => "LineString",
+            "coordinates" => [[30.0, -30.0], [90.0, -90.0]]
+          }
+        ],
+        "crs" => %{"type" => "name", "properties" => %{"name" => "EPSG:4326"}}
+      }
+
+      result = Geometry.cast(collection_map)
+
+      assert {:ok, collection} = result
+      assert %Geo.GeometryCollection{} = collection
+      assert length(collection.geometries) == 2
+      assert Enum.at(collection.geometries, 0).__struct__ == Geo.Point
+      assert Enum.at(collection.geometries, 1).__struct__ == Geo.LineString
+      assert collection.srid == 4326
+    end
+
+    test "cast GeometryCollection from map with key: value syntax" do
+      collection_map = %{
+        type: "GeometryCollection",
+        geometries: [
+          %{
+            type: "Point",
+            coordinates: [30.0, -90.0]
+          },
+          %{
+            type: "LineString",
+            coordinates: [[30.0, -30.0], [90.0, -90.0]]
+          }
+        ],
+        crs: %{type: "name", properties: %{name: "EPSG:4326"}}
+      }
+
+      result = Geometry.cast(collection_map)
+
+      assert {:ok, collection} = result
+      assert %Geo.GeometryCollection{} = collection
+      assert length(collection.geometries) == 2
+      assert Enum.at(collection.geometries, 0).__struct__ == Geo.Point
+      assert Enum.at(collection.geometries, 1).__struct__ == Geo.LineString
+      assert collection.srid == 4326
+    end
   end
 end
